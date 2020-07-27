31b5766ab1ffbd815e0e64354af360fa10ffc15d
zmahfuz061318 <str053mgr@gmail.com>
2020-07-26 21:02:33 -0400

    Completed

--- sumAllInArray.js
+++ sumAllInArray.js
@@ -1,7 +1,15 @@
 //SUM ALL IN ARRAY
 
+
 const sumAllInArray = arr => {
 
+  let sum = 0;
+  arr.forEach(element => {
+    if(!isNaN(element)){
+      sum += parseInt(element);
+    }
+  });
+  return sum;
 }
 
 console.log(sumAllInArray(["-1", "a", 4, "-32", 8, "94"]))
